08dcad664397cb3e84aa7bbcac05f505e01694ec
Mr. Tuik <toufikmolla7074@gmail.com>
2026-09-22 10:28:58 +0530

    Update files (22 added)

---
+++ PathHelp@tuik/lib/quotes.js
@@ -0,0 +1,19 @@
+// Quote strip: rotates every 5 seconds. Add / edit quotes here ({ text, by }).
+export const QUOTES = [
+  { text: 'tuik is everywhere', by: '' },
+  { text: 'Chance favours only the prepared mind.', by: 'Louis Pasteur' },
+  { text: 'The important thing is not to stop questioning.', by: 'Albert Einstein' },
+  { text: 'Genius is one percent inspiration and ninety-nine percent perspiration.', by: 'Thomas Edison' },
+  { text: 'Well done is better than well said.', by: 'Benjamin Franklin' },
+  { text: 'Arise, awake, and stop not till the goal is reached.', by: 'Swami Vivekananda' },
+  { text: 'Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.', by: 'A. P. J. Abdul Kalam' },
+  { text: 'The best way to predict the future is to invent it.', by: 'Alan Kay' },
+  { text: 'If I have seen further, it is by standing on the shoulders of giants.', by: 'Isaac Newton' },
+  { text: 'Stay hungry, stay foolish.', by: 'Steve Jobs' },
+  { text: 'The journey of a thousand miles begins with a single step.', by: 'Lao Tzu' },
+  { text: 'It does not matter how slowly you go as long as you do not stop.', by: 'Confucius' },
+  { text: 'Nothing in life is to be feared, it is only to be understood.', by: 'Marie Curie' },
+  { text: 'First, do no harm.', by: 'Hippocrates' },
+  { text: "You can't cross the sea merely by standing and staring at the water.", by: 'Rabindranath Tagore' },
+  { text: 'Nothing will work unless you do.', by: 'Maya Angelou' },
+];
